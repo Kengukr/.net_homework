bcbd6ec51e2cdd5cd90c581b91a90fcb32a35398
Kostya <kkozlovskij317@gmail.com>
2025-05-16 03:37:13 +0300

    wre

---
+++ Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace task_new
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) { }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) { }
+    }
+}
